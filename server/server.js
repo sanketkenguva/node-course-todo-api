e79a65e335f9e84e9b1f85cf177f27b1f465bb48
Sanket Kumar Kenguva <sanket.kenguva@BRILLIO.COM>
2018-10-05 12:10:27 +0530

    setup app for heroku with bug fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/Todo');
+var {Todo} = require('./models/todo');
 //var {User} = require('./models/User');
 
 var app = express();
