51ff32d49779461a8ed526baeaa8f025f8e83658
Sanket Kumar Kenguva <sanket.kenguva@BRILLIO.COM>
2018-10-08 15:00:12 +0530

    bug fix

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
-const {Todo} = require('./../models/Todo');
+const {Todo} = require('./../models/todo');
 
 const todos = [{
     _id: new ObjectID(),
