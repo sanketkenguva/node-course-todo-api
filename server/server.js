dd1e5f82610fb0cd8105b8d2fe1c09fed2e2dcbc
Sanket Kumar Kenguva <sanket.kenguva@BRILLIO.COM>
2018-10-08 14:49:33 +0530

    setup app for heroku after npm install on root directory

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
+var {Todo} = require('./models/Todo');
 //var {User} = require('./models/User');
 
 var app = express();
